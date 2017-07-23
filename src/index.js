7e4e468e78dfbdcb667f87e80bc88eec243f36e7
Dmitry Danilov <odoilboss@gmail.com>
2017-07-23 18:32:08 +0500

    add our work section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import $ from "jquery";
 import { WOW } from "wowjs";
+import magnificPopup from "magnific-popup";
 
 $(() => {
     new WOW().init();
+});
+
+$(() => {
+    $("#work").magnificPopup({
+        delegate: "a",
+        type: "image",
+        gallery: {
+            enabled: true
+        }
+    });
 });
