7463ee64493fd6063e7ff4608834d4e3d337ce22
Dmitry Danilov <odoilboss@gmail.com>
2017-07-25 13:37:39 +0500

    add navigation bar and back to top button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import $ from "jquery";
+import bootstrap from "bootstrap";
 import { WOW } from "wowjs";
 import magnificPopup from "magnific-popup";
 import owlCarousel from "owl.carousel";
 import waypoints from "waypoints/lib/jquery.waypoints";
 import counterUp from "jquery.counterup";
+import easing from "jquery.easing";
 
+//init WOW plugin
 $(() => {
     new WOW().init();
 });
 
+//init magnificPopup plugin for work section
 $(() => {
     $("#work").magnificPopup({
         delegate: "a",
@@ -19,6 +23,7 @@ $(() => {
     });
 });
 
+//init owlCarousel plugin for team section
 $(() => {
     $("#team-members").owlCarousel({
         items: 3,
@@ -29,6 +34,7 @@ $(() => {
     });
 });
 
+//init owlCarousel plugin for customers section
 $(() => {
     $("#customers-testimonials").owlCarousel({
         items: 1,
@@ -39,6 +45,7 @@ $(() => {
     });
 });
 
+//init counterUp plugin for stats section
 $(() => {
     $(".counter").counterUp({
         delay: 10,
@@ -46,6 +53,7 @@ $(() => {
     });
 });
 
+//init owlCarousel plugin for clients section
 $(() => {
     $("#clients-list").owlCarousel({
         items: 6,
@@ -53,5 +61,29 @@ $(() => {
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true
+    });
+});
+
+// show/hide transparent black navigation
+$(() => {
+    $(window).scroll(() => {
+        if ($(window).scrollTop() < 50) {
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+        } else {
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+
+// setup smooth scrolling
+$(() => {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+        const section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
     });
 });
